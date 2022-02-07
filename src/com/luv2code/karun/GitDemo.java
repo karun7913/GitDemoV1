487e4a4036f64b696dcf84b1d68c8544a74d4d55
WELCOME <WELCOME@192.168.0.6>
2022-02-07 09:27:56 +0530

    First commit

---
+++ src/com/luv2code/karun/GitDemo.java
@@ -0,0 +1,11 @@
+package com.luv2code.karun;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("karun Git");
+	}
+
+}
