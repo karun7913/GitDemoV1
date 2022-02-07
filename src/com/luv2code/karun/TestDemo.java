799211f27bb5810f100d2ec8d538a9f959eea9b8
WELCOME <WELCOME@192.168.0.6>
2022-02-07 13:15:12 +0530

    Added TestDemo to BranchX

---
+++ src/com/luv2code/karun/TestDemo.java
@@ -0,0 +1,10 @@
+package com.luv2code.karun;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TestDemo");
+	}
+
+}
